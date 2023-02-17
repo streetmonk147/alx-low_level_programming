295d471b0d3e61116613792c366b04b59278f8f6
babaseun lawal <lawalbabatunde147@yahoo.com>
2023-02-16 18:12:12 -0800

    Size of C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - Print the size of various types on the computer.
+ * main - prints the size of various types on the computer it is compiled and run on.
  * Return 0 (Success)
  */
 int main(void)
+
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
