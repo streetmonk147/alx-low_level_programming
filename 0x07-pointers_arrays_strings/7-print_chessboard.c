0a835de64b07b0cbf6bca8154fe1eebfb1432583
babaseun lawal <lawalbabatunde147@yahoo.com>
2023-03-07 19:42:41 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * print_chessboard - print the dashboard
- * @a: array of characters
- *
- * Description: a function to display the chessboard
- * Return: No return
+ * print_chessboard - prints the chessboard
+ * @a: two dimension array to print
  */
 void print_chessboard(char (*a)[8])
 {
